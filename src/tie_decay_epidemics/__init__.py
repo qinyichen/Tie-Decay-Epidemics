8187bd8c07412a919bc057bbf023754a84404a95
Qinyi Chen <qinyichen@g.ucla.edu>
2020-04-30 14:30:11 -0700

    Initial commit

---
+++ src/tie_decay_epidemics/__init__.py
@@ -0,0 +1,14 @@
+"""Top-level package for Tie-Decay Epidemics."""
+
+# Do not edit this string manually, always use bumpversion
+# Details in CONTRIBUTING.md
+__version__ = "0.0.1"
+
+__author__ = "Qinyi Chen"
+__email__ = "qinyichen@ucla.edu"
+
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2020, Qinyi Chen"
+
+
+from .example import Example  # noqa: F401
