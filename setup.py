18471ee39c270694d1f3245a5660772b2febf08f
Qinyi Chen <qinyichen@g.ucla.edu>
2020-05-21 23:07:31 -0700

    Bump version: 0.0.2 → 0.0.3

--- setup.py
+++ setup.py
@@ -83,6 +83,6 @@ setup(
     url="https://github.com/qinyichen/tie-decay-epidemics",
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="0.0.2",
+    version="0.0.3",
     zip_safe=False,
 )
